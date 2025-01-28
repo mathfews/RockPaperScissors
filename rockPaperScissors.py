6c8284634b5e197ead64ca909e7e19af9d4b504f
mathfews <matheusfcw1010@gmail.com>
2025-01-28 16:17:34 -0300

    trying to implement a tutorial/rules feature

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,3 +1,28 @@
 import colorama
-from colorama import Style, Back, Fore
-print(f"{Style.BRIGHT}Rock, Paper and Scissors")
+from colorama import Style, Back, Fore, init
+init(autoreset=True)
+print(f"{Fore.YELLOW}{Style.BRIGHT}Rock, Paper and Scissors")
+# logic behind it
+askAgain = 0
+tutorial = str(input("Do you know the rules?(y/n) "))
+while tutorial != 0:
+    while tutorial == "n":
+        print("* A Rock beats scissors by smashing them")
+        print("* Scissors beats paper by cutting it")
+        print("* Paper beats rock by covering it")
+        askAgain = str(input("Did you understand?(y/n) "))
+        while askAgain != "y":
+          print("* A Rock beats scissors by smashing them")
+          print("* Scissors beats paper by cutting it")
+          print("* Paper beats rock by covering it")
+          askAgain = str(input("Did you know the rules?(y/n) "))
+          if askAgain == "y":
+            print("Okay, now you are ready to play!")
+            tutorial = 0
+          else:
+            askAgain = str(input("Do you know the rules?(y/n) "))
+    if tutorial == "y":
+        print("do you know???????")
+        tutorial = str(input("Do you know the rules?(y/n) "))
+    else:
+        tutorial = str(input("Do you know the rules?(y/n) "))
