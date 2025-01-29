eca9ad2ce62a0bb58236384dcfbdb6e0d230a0d9
mathfews <matheusfcw1010@gmail.com>
2025-01-29 00:47:23 -0300

    implemented tutorial feature

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Rock, Papper and Scissors Python Project
 **Hello!👋** Here's a simple project to test myself as I start programming. The goal is to practice features like **sorting random strings from lists, using loops, checking winners, creating a scoreboard, adding difficulty levels, creating tutorials**, and improving the **user experience** in terminal. And, of course, to pratice using **Git and GitHub**. 
-- [ ] Create a tutorial page
+- [ x ] Create a tutorial page
 - [ ] Checking winners
 - [ ] Scoreboard
-- [ ] Difficulty Levels 
+- [ ] Difficulty Levels
 ![Rock, Paper and Scissors](https://media2.dev.to/dynamic/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fthepracticaldev.s3.amazonaws.com%2Fi%2Fvg6mmkv6hipscpi7pl0h.png)
--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,12 +1,30 @@
 import colorama, time
 from colorama import Style, Back, Fore, init
 init(autoreset=True)
-print(f"{Fore.YELLOW}{Style.BRIGHT}Rock, Paper and Scissors")
-tutorial = str(input("Do you know the rules?(y/n) "))
+print(f"-->{Fore.YELLOW}{Style.BRIGHT}Rock, Paper and Scissors{Style.RESET_ALL}<--")
+tutorial = str(input("$ Do you know the rules?(y/n) "))
+tutorial1 = 0
 rules = {
-   "Rule 1" : "A Rock beats scissors by smashing them",
-   "Rule 2" : "Scissors beats paper by cutting it",
-   "Rule 3" : "Paper beats rock by covering it",
+   "Rule 1" : f"A {Fore.YELLOW}{Style.BRIGHT}Rock beats scissors{Style.RESET_ALL} by smashing them",
+   "Rule 2" : f"{Fore.YELLOW}{Style.BRIGHT}Scissors beats paper{Style.RESET_ALL} by cutting it",
+   "Rule 3" : f"{Fore.YELLOW}{Style.BRIGHT}Paper beats rock{Style.RESET_ALL} by covering it",
 }
 while tutorial != 0:
-    
+    if tutorial != "y" and tutorial != "n":
+        tutorial = str(input("$ Do you know the rules?(y/n) "))
+    if tutorial == "n":
+        for value in rules.values():
+            print(f"| {value} |")
+            time.sleep(0.5)
+        tutorial1 = str(input("$ Did you understand the rules?(y/n) "))
+        while tutorial1 != "y" and tutorial1 != "n":
+          tutorial1 = str(input("$ Did you understand the rules?(y/n) "))
+        if tutorial1 == "y":
+            tutorial = "y"
+        elif tutorial1 == "n":
+          print("Okay, I'll repeat it for you")
+          time.sleep(0.5)
+          tutorial = "n"
+    if tutorial == "y":
+        print("Okay, let's play")
+        exit()
