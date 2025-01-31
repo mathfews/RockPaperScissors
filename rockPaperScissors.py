1756cfeb41ac2e5646313d2df66aebc12592f7d5
mathfews <matheusfcw1010@gmail.com>
2025-01-31 09:33:09 -0300

    adding difficulty levels

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,13 +1,14 @@
-import colorama, time
+import colorama,time,random
 from colorama import Style, Back, Fore, init
 init(autoreset=True)
-print(f"-->{Fore.YELLOW}{Style.BRIGHT}Rock, Paper and Scissors{Style.RESET_ALL}<--")
+print(f"{Fore.YELLOW}{Style.BRIGHT}# Rock, Paper and Scissors")
 understood = 0
 rules = [
    f"A {Fore.YELLOW}{Style.BRIGHT}Rock beats scissors{Style.RESET_ALL} by smashing them",
    f"{Fore.YELLOW}{Style.BRIGHT}Scissors beats paper{Style.RESET_ALL} by cutting it",
    f"{Fore.YELLOW}{Style.BRIGHT}Paper beats rock{Style.RESET_ALL} by covering it"
 ]
+possibleMoves = ["Rock", "Paper","Scissors"]
 def ask(text):
    while True:
     ask = input(text)
@@ -23,3 +24,21 @@ if ask("Do you want to know the rules?(y/n) "):
       understood = ask("Did you understand the rules?(y/n) ")
       if understood:
         break
+while True:
+  print("Select difficulty:")
+  time.sleep(0.5)
+  print("- Easy(E)")
+  time.sleep(0.5)
+  print("- Normal(N)")
+  time.sleep(0.5)
+  print(f"- {Fore.RED}{Style.BRIGHT}Impossible(I)")
+  level = input("$ ").lower()
+  if level == "n" or level == "normal":
+    print(f"You choose {Style.BRIGHT}normal{Style.RESET_ALL} difficulty!")
+    break
+  if level == "e" or level == "easy":
+    print(f"You choose {Style.BRIGHT}easy{Style.RESET_ALL} difficulty!")
+    break
+  if level == "i" or level == "impossible":
+    print(f"You choose {Fore.RED}{Style.BRIGHT}IMPOSSIBLE{Style.RESET_ALL} difficulty!")
+    break
